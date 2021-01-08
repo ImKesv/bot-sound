7e6ffbac152fbd1c5e3d15b8e92e25bed02616ac
ImKesv <77145200+ImKesv@users.noreply.github.com>
2021-01-08 17:25:50 +0700

    wow bot

---
+++ sound.py
@@ -0,0 +1,9 @@
+import pyttsx3
+import PyPDF2
+speaker = pyttsx3.init()
+
+"""u can change it
+must be inside speaker.say() function"""
+
+speaker.say("""BOOM HEADSHOT!!!!!!!!!""")
+speaker.runAndWait()
